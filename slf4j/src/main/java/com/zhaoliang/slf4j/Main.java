46a327ad80a1b1440f9313c86b54a5227ff1052a
weston <weston_contribute@163.com>
2016-04-17 17:22:40 +0800

    slf4j修改。

--- slf4j/src/main/java/com/zhaoliang/slf4j/Main.java
+++ slf4j/src/main/java/com/zhaoliang/slf4j/Main.java
@@ -4,29 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 
  * @author Administrator
- *
  */
 public class Main {
-	public static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) {
-		logger.info("hello");
-		logger.info("Temperature set to {}. Old temperature was {}.", 37, 40);
-		logger.debug("Temperature set to {}. Old temperature was {}.", 37, 40);
-		logger.isDebugEnabled();
-		System.out.print("hello world");
-		logger.debug("Temperature set to {}. Old temperature was {}.", 37, 40);
-		
-		Exception exception = new Exception("cuowu");
-		logger.error(exception.getMessage(),exception);
-		
-		
-		if(logger.isTraceEnabled()){
-			logger.trace("message");
-		}
-	}
-	
+    public static void main(String[] args) {
+        logger.info("hello");
+        logger.info("Temperature set to {}. Old temperature was {}.", 37, 40);
+        logger.debug("Temperature set to {}. Old temperature was {}.", 37, 40);
+        logger.isDebugEnabled();
+        System.out.print("hello world");
+        logger.debug("Temperature set to {}. Old temperature was {}.", 37, 40);
+
+        Exception exception = new Exception("error");
+        logger.error(exception.getMessage(), exception);
+
+
+        if (logger.isTraceEnabled()) {
+            logger.trace("message");
+        }
+    }
+
 
 }
--- slf4j/src/main/java/com/zhaoliang/slf4j/SLForJ.java
+++ slf4j/src/main/java/com/zhaoliang/slf4j/SLForJ.java
@@ -4,25 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SLForJ {
-	public final Logger logger = LoggerFactory.getLogger(SLForJ.class);
-	Integer temp;
-	Integer oldT;
+    public final Logger logger = LoggerFactory.getLogger(SLForJ.class);
+    Integer temp;
+    Integer oldT;
 
-	public void setTemperature(Integer temperature) {
-		oldT = temp;
-		temp = temperature;
-		Object[] objs = { new java.util.Date().toString(), temp, oldT };
-		logger.info(
-				"Today is {}, Temperature set to {}. Old temperature was {}.",
-				objs);
-		if (temperature.intValue() > 50) {
-			logger.warn("Temperature({}) has risen above 50 degrees.", temp);
-		}
-	}
+    public static void main(String[] args) {
+        SLForJ wombat = new SLForJ();
+        wombat.setTemperature(10);
+        wombat.setTemperature(60);
+    }
 
-	public static void main(String[] args) {
-		SLForJ wombat = new SLForJ();
-		wombat.setTemperature(10);
-		wombat.setTemperature(60);
-	}
+    public void setTemperature(Integer temperature) {
+        oldT = temp;
+        temp = temperature;
+        Object[] objs = {new java.util.Date().toString(), temp, oldT};
+        logger.info(
+            "Today is {}, Temperature set to {}. Old temperature was {}.",
+            objs);
+        if (temperature.intValue() > 50) {
+            logger.warn("Temperature({}) has risen above 50 degrees.", temp);
+        }
+    }
 }
