c1bf380fd5cfd72e97b909d8e8a8176da7179f90
weston <weston_contribute@163.com>
2016-04-18 09:08:34 +0800

    采用java8中lambda方式。

--- thread/src/main/java/com/zhaoliang/thread/study/Executors/ExecutorTest.java
+++ thread/src/main/java/com/zhaoliang/thread/study/Executors/ExecutorTest.java
@@ -9,24 +9,20 @@ public class ExecutorTest {
 	private static void run(ExecutorService threadPool) {
 		for (int i = 1; i < 5; i++) {
 			final int taskID = i;
-			threadPool.execute(new Runnable() {
-				@Override
-				public void run() {
-					for (int i = 1; i < 5; i++) {
-						try {
-							Thread.sleep(20);// 为了测试出效果，让每次任务执行都需要一定时间
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						System.out.println("第" + taskID + "次任务的第" + i + "次执行");
-					}
-				}
-			});
+			threadPool.execute(() -> {
+                for (int i1 = 1; i1 < 5; i1++) {
+                    try {
+                        Thread.sleep(20);// 为了测试出效果，让每次任务执行都需要一定时间
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("第" + taskID + "次任务的第" + i1 + "次执行");
+                }
+            });
 		}
 		threadPool.shutdown();// 任务执行完毕，关闭线程池
 	}
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		// 创建可以容纳3个线程的线程池
 		ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
