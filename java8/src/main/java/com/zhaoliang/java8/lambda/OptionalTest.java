378fee188a19986fe2e9def4b466b65f4e4f4775
weston <weston_contribute@163.com>
2016-04-23 22:36:57 +0800

    Java8中optinal类的demo.

---
+++ java8/src/main/java/com/zhaoliang/java8/lambda/OptionalTest.java
@@ -0,0 +1,7 @@
+package com.zhaoliang.java8.lambda;
+
+/**
+ * Created by zhaoliang(weston_contribute@163.com) on 2016/4/23.
+ */
+public class OptionalTest {
+}
---
+++ java8/src/test/java/com/zhaoliang/java8/lambda/OptionalTestTest.java
@@ -0,0 +1,22 @@
+package com.zhaoliang.java8.lambda;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Created by zhaoliang(weston_contribute@163.com) on 2016/4/23.
+ */
+public class OptionalTestTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
