c36a026f1ef5f0ce4ea43e49748024d14ef8914d
weston zhao <weston_contribute@163.com>
2016-04-25 17:13:13 +0800

    Update OptionalTest.java
    modify

--- java8/src/main/java/com/zhaoliang/java8/lambda/OptionalTest.java
+++ java8/src/main/java/com/zhaoliang/java8/lambda/OptionalTest.java
@@ -4,6 +4,8 @@ package com.zhaoliang.java8.lambda;
  * Created by zhaoliang(weston_contribute@163.com) on 2016/4/23.
  */
 public class OptionalTest {
+    
+    private static String test = "test";
     public static void main(String[] args) {
 
     }
