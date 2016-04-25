732f5ddd80021ab5e1d97b13f722221ea86b9692
weston zhao <weston_contribute@163.com>
2016-04-25 17:23:41 +0800

    Update OptionalTest.java
    make confiut

--- java8/src/main/java/com/zhaoliang/java8/lambda/OptionalTest.java
+++ java8/src/main/java/com/zhaoliang/java8/lambda/OptionalTest.java
@@ -8,6 +8,7 @@ public class OptionalTest {
     private static String test = "test";
     public static void main(String[] args) {
         System.out.println(test);
+        System.out.println(test.equals("test"))
         System.out.println();
         System.out.println();
         System.out.println();
