8ad862ce1820f65c675765e5d5819a32202e305a
weston <weston_contribute@163.com>
2016-06-15 16:22:57 +0800

    maven-assembly-plugin demo

---
+++ plugin-test/src/main/java/com/zhaoliang/plugin/Test.java
@@ -0,0 +1,15 @@
+package com.zhaoliang.plugin;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * add a class.
+ *
+ * Created by zhaoliang(weston_contribute@163.com) on 2016/6/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(StringUtils.EMPTY);
+    }
+}
