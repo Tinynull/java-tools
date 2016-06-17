213e9d872fffb74502e410683cd8f711dc1b3dc6
weston <weston_contribute@163.com>
2016-06-17 16:27:13 +0800

    泛型

--- java8/src/main/java/com/zhaoliang/generic/Demo.java
+++ java8/src/main/java/com/zhaoliang/generic/Demo.java
@@ -1,7 +1,34 @@
 package com.zhaoliang.generic;
 
+import static javafx.scene.input.KeyCode.T;
+
 /**
+ * 泛型。
+ *
  * Created by zhaoliang(weston_contribute@163.com) on 2016/6/16.
  */
-public class Demo {
+public class Demo<E> {
+
+
+    public E fun2(E t) {
+        return t;
+    }
+
+    public <T> T fun(T t) {
+        return t;
+    }
+
+    public <T extends Number> T fun3(T t) {
+        return t;
+    }
+
+
+    public static void main(String[] args) {
+        Demo<String> demo = new Demo<>();
+        System.out.println("demo.fun(2.3) = " + demo.fun(2.3));
+        System.out.println("demo.fun2(\"hello\") = " + demo.fun2("hello"));
+        System.out.println("demo.fun3(\"hello\".hashCode()) = " + demo.fun3("hello".hashCode()));
+    }
+
+
 }
