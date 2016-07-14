1e304b60f4cbf0df3f091395e381a28c5d9f203b
weston <weston_contribute@163.com>
2016-07-14 11:08:36 +0800

    maven-assembly demo

---
+++ maven-assembly/src/main/java/com/zhaoliang/maven/assembly/Test.java
@@ -0,0 +1,10 @@
+package com.zhaoliang.maven.assembly;
+
+/**
+ * Created by zhaoliang(weston_contribute@163.com) on 2016/7/14.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
