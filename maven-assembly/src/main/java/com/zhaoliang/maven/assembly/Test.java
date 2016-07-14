3750bb5b3830cc3d43dfd48c78d422c3904d7f25
weston zhao <weston_contribute@163.com>
2016-07-14 15:17:19 +0800

    Update Test.java
    modify

--- maven-assembly/src/main/java/com/zhaoliang/maven/assembly/Test.java
+++ maven-assembly/src/main/java/com/zhaoliang/maven/assembly/Test.java
@@ -4,7 +4,11 @@ package com.zhaoliang.maven.assembly;
  * Created by zhaoliang(weston_contribute@163.com) on 2016/7/14.
  */
 public class Test {
+    public Test(){}
     public static void main(String[] args) {
         System.out.println("hello");
+    }
+    public static void test2(){
+        System.out.println("test2");
     }
 }
