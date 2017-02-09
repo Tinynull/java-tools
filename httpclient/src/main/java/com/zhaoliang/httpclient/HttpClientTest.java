657dca72d725f0fb49b8104a741e76739b94de6e
zhaoliang <weston_contribute@163.com>
2017-02-09 16:55:50 +0800

    add demo for httpclient

---
+++ httpclient/src/main/java/com/zhaoliang/httpclient/HttpClientTest.java
@@ -0,0 +1,227 @@
+package com.zhaoliang.httpclient;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * test
+ * Created by (zhaoliang@metagraph.io) on (17-2-9).
+ */
+public class HttpClientTest {
+       /**
+     * HttpClient连接SSL
+     */
+    public void ssl() {
+        CloseableHttpClient httpclient = null;
+        try {
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            FileInputStream inStream = new FileInputStream(new File("d:\\tomcat.keystore"));
+            try {
+                // 加载keyStore d:\\tomcat.keystore
+                trustStore.load(inStream, "123456".toCharArray());
+            } catch (CertificateException | NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    inStream.close();
+                } catch (Exception ignore) {
+                }
+            }
+            // 相信自己的CA和所有自签名的证书
+            SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(trustStore, new TrustSelfSignedStrategy()).build();
+            // 只允许使用TLSv1协议
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new String[] { "TLSv1" }, null,
+                    SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+            httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+            // 创建http请求(get方式)
+            HttpGet httpget = new HttpGet("https://localhost:8443/myDemo/Ajax/serivceJ.action");
+            System.out.println("executing request" + httpget.getRequestLine());
+            try (CloseableHttpResponse response = httpclient.execute(httpget)) {
+                HttpEntity entity = response.getEntity();
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    System.out.println("Response content length: " + entity.getContentLength());
+                    System.out.println(EntityUtils.toString(entity));
+                    EntityUtils.consume(entity);
+                }
+            }
+        } catch (ParseException | IOException | NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            e.printStackTrace();
+        } finally {
+            if (httpclient != null) {
+                try {
+                    httpclient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * post方式提交表单（模拟用户登录请求）
+     */
+    public void postForm() {
+        // 创建默认的httpClient实例.
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建httppost
+        HttpPost httppost = new HttpPost("http://localhost:8080/myDemo/Ajax/serivceJ.action");
+        // 创建参数队列
+        List<NameValuePair> formparams = new ArrayList<>();
+        formparams.add(new BasicNameValuePair("username", "admin"));
+        formparams.add(new BasicNameValuePair("password", "123456"));
+        UrlEncodedFormEntity uefEntity;
+        try {
+            uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");
+            httppost.setEntity(uefEntity);
+            System.out.println("executing request " + httppost.getURI());
+            try (CloseableHttpResponse response = httpclient.execute(httppost)) {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    System.out.println("--------------------------------------");
+                    System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));
+                    System.out.println("--------------------------------------");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 发送 post请求访问本地应用并根据传递参数不同返回不同结果
+     */
+    public void post() {
+        // 创建默认的httpClient实例.
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建httpPost
+        HttpPost httpPost = new HttpPost("http://localhost:8080/myDemo/Ajax/serivceJ.action");
+        // 创建参数队列
+        List<NameValuePair> formParams = new ArrayList<>();
+        formParams.add(new BasicNameValuePair("type", "house"));
+        UrlEncodedFormEntity uefEntity;
+        try {
+            uefEntity = new UrlEncodedFormEntity(formParams, "UTF-8");
+            httpPost.setEntity(uefEntity);
+            System.out.println("executing request " + httpPost.getURI());
+            try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    System.out.println("--------------------------------------");
+                    System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));
+                    System.out.println("--------------------------------------");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 发送 get请求
+     */
+    public void get() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            // 创建httpget.
+            HttpGet httpget = new HttpGet("http://www.baidu.com/");
+            System.out.println("executing request " + httpget.getURI());
+            // 执行get请求.
+            try (CloseableHttpResponse response = httpclient.execute(httpget)) {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                System.out.println("--------------------------------------");
+                // 打印响应状态
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    // 打印响应内容长度
+                    System.out.println("Response content length: " + entity.getContentLength());
+                    // 打印响应内容
+                    System.out.println("Response content: " + EntityUtils.toString(entity));
+                }
+                System.out.println("------------------------------------");
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 上传文件
+     */
+    public void upload() throws IOException {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost httppost = new HttpPost("http://localhost:8080/myDemo/Ajax/serivceFile.action");
+
+            FileBody bin = new FileBody(new File("F:\\image\\sendpix0.jpg"));
+            StringBody comment = new StringBody("A binary file of some kind", ContentType.TEXT_PLAIN);
+
+            HttpEntity reqEntity = MultipartEntityBuilder.create().addPart("bin", bin).addPart("comment", comment).build();
+
+            httppost.setEntity(reqEntity);
+
+            System.out.println("executing request " + httppost.getRequestLine());
+            try (CloseableHttpResponse response = httpclient.execute(httppost)) {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                HttpEntity resEntity = response.getEntity();
+                if (resEntity != null) {
+                    System.out.println("Response content length: " + resEntity.getContentLength());
+                }
+                EntityUtils.consume(resEntity);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
